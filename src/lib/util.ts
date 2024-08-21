186ae044eeb6e4051743bec7035d9c43cff3ed4c
Amir ali <amiralikhamseh01@gmail.com>
2024-08-21 20:00:08 +0200

    feat: message db schema and functionality implemented

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,4 @@
-import { differenceInYears } from "date-fns";
+import { differenceInYears, format } from "date-fns";
 import { FieldValues, Path, UseFormSetError } from "react-hook-form";
 import { ZodIssue } from "zod";
 
@@ -31,4 +31,16 @@ export function transformImageUrl(imageUrl?: string | null) {
   return `${imageUrl.slice(0, uploadIndex)}${transformation}${imageUrl.slice(
     uploadIndex
   )}`;
+}
+
+export function formatShortDateTime(date: Date) {
+  return format(date, "dd MMM yy h:mm:a");
+}
+
+export function truncateString(text?: string | null, num = 50) {
+  if (!text) return null;
+  if (text.length <= num) {
+    return text;
+  }
+  return text.slice(0, num) + "...";
 }
