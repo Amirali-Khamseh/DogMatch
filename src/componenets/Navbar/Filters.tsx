257a465c3a63f7df2bbac7f65a7e0352a120e931
Amir ali <amiralikhamseh01@gmail.com>
2024-08-27 14:48:40 +0200

    feat: silters component created

---
+++ src/componenets/Navbar/Filters.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import {
+  Button,
+  Select,
+  SelectItem,
+  Slider,
+  Spinner,
+  Switch,
+} from "@nextui-org/react";
+import { useFilters } from "../../../hooks/useFilters";
+
+export default function Filters() {
+  const {
+    genderList,
+    orderByList,
+    filters,
+    selectAge,
+    selectGender,
+    selectOrder,
+    clientLoaded,
+    isPending,
+    totalCount,
+    selectWithPhoto,
+  } = useFilters();
+
+  return (
+    <div className="shadow-md py-2">
+      <div className="flex flex-row justify-around items-center gap-4 w-full">
+        <div className="flex gap-2 items-center">
+          <div className=" font-semibold text-xl">
+            Results: {isPending ? <Spinner size="sm" /> : totalCount}
+          </div>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <div>Gender:</div>
+          {genderList.map(({ icon: Icon, value }) => (
+            <Button
+              key={value}
+              size="sm"
+              isIconOnly
+              color={filters.gender.includes(value) ? "primary" : "default"}
+              onClick={() => selectGender(value)}
+            >
+              <Icon size={24} />
+            </Button>
+          ))}
+        </div>
+        <div className="flex flex-row items-center gap-2 w-1/4">
+          <Slider
+            label={clientLoaded && "Age range"}
+            size="sm"
+            minValue={18}
+            maxValue={100}
+            defaultValue={filters.ageRange}
+            onChangeEnd={(value) => selectAge(value as number[])}
+            aria-label="Age range slider"
+          />
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="text-sm">With photo</p>
+          <Switch defaultSelected size="sm" onChange={selectWithPhoto} />
+        </div>
+        <div className="w-1/4">
+          <Select
+            size="sm"
+            fullWidth
+            label="Order by"
+            variant="bordered"
+            aria-label="Order by selector"
+            selectedKeys={new Set([filters.orderBy])}
+            onSelectionChange={selectOrder}
+          >
+            {orderByList.map((item) => (
+              <SelectItem key={item.value} value={item.value}>
+                {item.label}
+              </SelectItem>
+            ))}
+          </Select>
+        </div>
+      </div>
+    </div>
+  );
+}
